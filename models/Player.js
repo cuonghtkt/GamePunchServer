8e15452dc4bfaab03d81b90ec2834f3609274fac
cuonghtkt <cuong01629434877@gmail.com>
2022-02-19 09:57:57 +0700

    server 1.0

---
+++ models/Player.js
@@ -0,0 +1,39 @@
+const { type } = require("express/lib/response");
+
+module.exports = class Player {
+    constructor() {
+        this.username = '';
+        this.id = '';
+        this.position = new Vector2();
+        this.rotation = 0;
+        this.status = new Status();
+    }
+    new() {
+        this.position = new Vector2();
+        this.status = new Status();
+    }
+    statusToString() {
+        return this.id + "|" + this.status.toString();
+    }
+    positionToString() {
+        return this.id + "|" + this.position.toString();
+    }
+}
+class Vector2 {
+    constructor(X = 0, Y = 0) {
+        this.x = X;
+        this.y = Y;
+    }
+    toString() {
+        return this.x + "|" + this.y;
+    }
+}
+class Status {
+    constructor(hp = 10) {
+        this.hp = hp;
+    }
+
+    toString() {
+        return this.hp;
+    }
+}
